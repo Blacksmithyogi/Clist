cf7e989ce4d234acf7cd564eef820571b3c9c486
Blacksmithyogi <ysweater@gmail.com>
2023-09-02 02:56:47 +0800

    redesign bg

--- app/src/main/java/com/giyo/anu/PomAdapter.kt
+++ app/src/main/java/com/giyo/anu/PomAdapter.kt
@@ -17,19 +17,18 @@ class PomAdapter(
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val inflater: LayoutInflater = LayoutInflater.from(context)
-        val view: View = inflater.inflate(R.layout.list_item, null)
+        val view = convertView ?: createView(parent, position)
+        /*val inflater: LayoutInflater = LayoutInflater.from(context)
+        val view: View = inflater.inflate(R.layout.list_item, null)*/
 
         val imageView: ImageView = view.findViewById(R.id.pom_ava)
         val pName: TextView = view.findViewById(R.id.pom_title)
         val pSummary: TextView = view.findViewById(R.id.pom_summary)
-        val lastPosition = arrayList.size - 1
 
         imageView.setImageResource(arrayList[position].pomImage)
         pName.text = arrayList[position].pomName
         pSummary.text = arrayList[position].pomSummary
-        if (position == 0) {
+        /*if (position == 0) {
             view.setBackgroundResource(R.drawable.card_top)
         } else if (position > 0) {
             view.setBackgroundResource(R.drawable.card_middle)
@@ -38,10 +37,22 @@ class PomAdapter(
         // Check if it's the last item and set a specific color
         if (position == lastPosition) {
             view.setBackgroundResource(R.drawable.card_bot) // Change to your desired color
-        }
+        }*/
 
         return view
 
+    }
+
+    private fun createView(parent: ViewGroup, position: Int): View {
+        val user = arrayList[position]
+
+        val layoutId = when {
+            user.pomName == "Coco" -> R.layout.pom_top
+            user.pomName == "Shiro" -> R.layout.pom_bot
+
+            else -> R.layout.pom_mid
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false)
     }
 
 }
